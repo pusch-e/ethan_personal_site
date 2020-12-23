4771a2dde7569b93e7f2a593d017bef940466dc8
pusch-e <56702540+pusch-e@users.noreply.github.com>
2020-12-22 23:12:48 -0500

    header altered a lot. Much closer to final version of header

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Link from "next/link"
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faAddressCard, faFile, faChevronRight, faCode } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faAddressCard, faFile, faBriefcase, faCode, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Image from "next/image"
 
-library.add(faAddressCard, faFile, faChevronRight, faCode)
+library.add(faAddressCard, faFile, faBriefcase, faCode, faEnvelope, fab)
 
 const Navbar = () => {
     return (
@@ -12,11 +14,16 @@ const Navbar = () => {
         <nav className="navbar">
           <ul className="navbar-nav">
 
-            <li className="nav-name">
+            <li className="nav-item">
+                <div className="nav-link" id="nav-link-pic">
+                    <Image src="/emanemotemed.png" width={55} height={55} className="self-picture"/>
+                </div>
+            </li>
+
+            <li className="nav-name-item">
                 <Link href="/home">
-                    <a className="nav-link">
-                        <span className="link-text name-text"> Ethan </span>
-                        <FontAwesomeIcon className="fa-icon fa-icon-chevron" icon="chevron-right" fixedWidth/>
+                    <a className="nav-name-link">
+                        <span className="name-text"> Ethan Pusch </span>
                     </a>
                 </Link>
             </li>
@@ -26,6 +33,15 @@ const Navbar = () => {
                 <a className="nav-link">
                   <FontAwesomeIcon className="fa-icon" icon="address-card" fixedWidth/>
                   <span className="link-text">About</span>
+                </a>
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link href="/experience">
+                <a className="nav-link">
+                  <FontAwesomeIcon className="fa-icon" icon="briefcase" fixedWidth/>
+                  <span className="link-text">Experience</span>
                 </a>
               </Link>
             </li>
@@ -46,6 +62,28 @@ const Navbar = () => {
                   <span className="link-text">Resume</span>
                 </a>
               </Link>
+            </li>
+            
+            <li className="nav-item">
+              <div className="external-links">
+                <Link href="https://github.com/pusch-e">
+                  <a className="external-link" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon className="fa-icon" icon={['fab', 'github']} fixedWidth/>
+                  </a>
+                </Link>
+
+                <Link href="https://www.linkedin.com/in/ethanpusch">
+                  <a className="external-link" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon className="fa-icon" icon={['fab', 'linkedin']} fixedWidth/>
+                  </a>
+                </Link>
+
+                <Link href="mailto:pusch.e@northeastern.edu">
+                  <a className="external-link">
+                    <FontAwesomeIcon className="fa-icon" icon="envelope" fixedWidth/>
+                  </a>
+                </Link>
+              </div>
             </li>
 
           </ul>
---
+++ src/pages/experience.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Link from "next/link"
+import Navbar from "../components/navbar"
+
+const Experience = () => {
+    return (
+        <div>
+            <Navbar/>
+
+            <div className="main">
+                <h1> Experience </h1>
+            </div>
+        </div>
+    );
+}
+  
+export default Experience;
