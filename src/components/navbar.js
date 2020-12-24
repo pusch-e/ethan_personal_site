c4bfd2af46b81b75e42e6b9e03d941dc55a52d74
pusch-e <56702540+pusch-e@users.noreply.github.com>
2020-12-23 22:29:11 -0500

    removed duplicate code by use .map

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,104 +10,112 @@ library.add(faAddressCard, faFile, faBriefcase, faCode, faEnvelope, fab)
 
 const Navbar = () => {
 
-    const [toggleHover, setToggleHover] = useState(false);
-    const [toggleHover2, setToggleHover2] = useState(false);
-    const [toggleHover3, setToggleHover3] = useState(false);
-    const [toggleHover4, setToggleHover4] = useState(false);
+    const [toggleHoverAbout, setToggleHoverAbout] = useState(false);
+    const [toggleHoverExp, setToggleHoverExp] = useState(false);
+    const [toggleHoverProj, setToggleHoverProj] = useState(false);
+    const [toggleHoverRes, setToggleHoverRes] = useState(false);
+
+    const pageNavItems = [
+        {
+            setHover: setToggleHoverAbout,
+            isHover: toggleHoverAbout,
+            linkTo: "/about",
+            icon: "address-card",
+            linkText: "About"
+        },
+        {
+            setHover: setToggleHoverExp,
+            isHover: toggleHoverExp,
+            linkTo: "/experience",
+            icon: "briefcase",
+            linkText: "Experience"
+        },
+        {
+            setHover: setToggleHoverProj,
+            isHover: toggleHoverProj,
+            linkTo: "/projects",
+            icon: "code",
+            linkText: "Projects"
+        },
+        {
+            setHover: setToggleHoverRes,
+            isHover: toggleHoverRes,
+            linkTo: "/about",
+            icon: "file",
+            linkText: "Resume"
+        }
+    ]
+
+    const externalNavItems = [
+        {
+            linkTo: "https://github.com/pusch-e",
+            icon: ['fab', 'github']
+        },
+        {
+            linkTo: "https://www.linkedin.com/in/ethanpusch",
+            icon: ['fab', 'linkedin']
+        },
+        {
+            linkTo: "mailto:pusch.e@northeastern.edu",
+            icon: "envelope"
+        }
+    ]
 
     return (
-      <div>
-        <nav className="navbar">
-          <ul className="navbar-nav">
+        <div>
+            <nav className="navbar">
+                <ul className="navbar-nav">
 
-            <li className="nav-item">
-                <div className="nav-link">
-                    <Image src="/emanemotemed.png" width={55} height={55} className="self-picture"/>
-                </div>
-            </li>
+                    <li className="nav-item">
+                        <div className="nav-link">
+                            <Image src="/emanemotemed.png" width={50} height={50} className="self-picture"/>
+                        </div>
+                    </li>
 
-            <li className="nav-name-item">
-                <Link href="/home">
-                    <a className="nav-link" id="nav-name-link">
-                        <span className="name-text"> Ethan Pusch </span>
-                    </a>
-                </Link>
-            </li>
+                    <li className="nav-name-item">
+                        <Link href="/home">
+                            <a className="nav-link" id="nav-name-link">
+                                <span className="name-text"> Ethan Pusch </span>
+                            </a>
+                        </Link>
+                    </li>
 
-            <li className="nav-item" onMouseEnter={() => setToggleHover(true)} onMouseLeave={() => setToggleHover(false)}>
-              <Link href="/about">
-                <a className="nav-link">
-                  { toggleHover ?
-                  <FontAwesomeIcon className="fa-icon" icon="address-card" fixedWidth/>
-                  :
-                  <span className="link-text">About</span>
-                  }
-                </a>
-              </Link>
-            </li>
+                    {
+                        pageNavItems.map( (pageNavItem, i) =>
+                            <li key={i} className="nav-item" 
+                             onMouseEnter={() => pageNavItem.setHover(true)}
+                             onMouseLeave={() => pageNavItem.setHover(false)}>
+                                <Link href={`${pageNavItem.linkTo}`}>
+                                    <a className="nav-link">
+                                        { pageNavItem.isHover ?
+                                        <FontAwesomeIcon className="fa-icon" icon={pageNavItem.icon} fixedWidth/>
+                                        :
+                                        <span className="link-text">{pageNavItem.linkText}</span>
+                                        }
+                                    </a>
+                                </Link>
+                            </li>
+                        )
+                    }
+                
+                    <li className="nav-item" id="nav-item-external-links">
+                        <div className="external-links">
+                            {
+                                externalNavItems.map( (externalNavItem, i) =>
+                                    <Link key={i} href={`${externalNavItem.linkTo}`}>
+                                        <a className="external-link" target="_blank" rel="noopener noreferrer">
+                                            <FontAwesomeIcon className="fa-icon-external" icon={externalNavItem.icon} fixedWidth/>
+                                        </a>
+                                    </Link>
+                                )
+                            }
+                        </div>
+                    </li>
 
-            <li className="nav-item" onMouseEnter={() => setToggleHover2(true)} onMouseLeave={() => setToggleHover2(false)}>
-              <Link href="/experience">
-                <a className="nav-link">
-                  { toggleHover2 ?
-                  <FontAwesomeIcon className="fa-icon" icon="briefcase" fixedWidth/>
-                  :
-                  <span className="link-text">Experience</span>
-                  }
-                </a>
-              </Link>
-            </li>
-
-            <li className="nav-item" onMouseEnter={() => setToggleHover3(true)} onMouseLeave={() => setToggleHover3(false)}>
-              <Link href="/projects">
-                <a className="nav-link">
-                  { toggleHover3 ?
-                  <FontAwesomeIcon className="fa-icon" icon="code" fixedWidth/>
-                  :
-                  <span className="link-text">Projects</span>
-                  }
-                </a>
-              </Link>
-            </li>
-
-            <li className="nav-item" onMouseEnter={() => setToggleHover4(true)} onMouseLeave={() => setToggleHover4(false)}>
-              <Link href="/about">
-                <a className="nav-link">
-                  { toggleHover4 ?
-                  <FontAwesomeIcon className="fa-icon" icon="file" fixedWidth/>
-                  :
-                  <span className="link-text">Resume</span>
-                  }
-                </a>
-              </Link>
-            </li>
-            
-            <li className="nav-item" id="nav-item-external-links">
-              <div className="external-links">
-                <Link href="https://github.com/pusch-e">
-                  <a className="external-link" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon className="fa-icon-external" icon={['fab', 'github']} fixedWidth/>
-                  </a>
-                </Link>
-
-                <Link href="https://www.linkedin.com/in/ethanpusch">
-                  <a className="external-link" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon className="fa-icon-external" icon={['fab', 'linkedin']} fixedWidth/>
-                  </a>
-                </Link>
-
-                <Link href="mailto:pusch.e@northeastern.edu">
-                  <a className="external-link">
-                    <FontAwesomeIcon className="fa-icon-external" icon="envelope" fixedWidth/>
-                  </a>
-                </Link>
-              </div>
-            </li>
-
-          </ul>
-        </nav>
-      </div>
+                </ul>
+            </nav>
+        </div>
     );
-  }
+}
   
-  export default Navbar;
+export default Navbar;
