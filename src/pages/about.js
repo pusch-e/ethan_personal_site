5aed9a8405a38b62da2afa0a56318a1b25769920
pusch-e <56702540+pusch-e@users.noreply.github.com>
2021-01-10 23:41:33 -0500

    Added content to about page and spaced out experience page

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,13 +17,35 @@ const About = () => {
             </div>
 
             <div className= "experience-container fade-in">
-                <h2>
-                    Hello! I'm Ethan Pusch, an aspiring software engineer and third-year student at Northeastern University.
-                    I'm currently based in Boston, MA, but grew up in the suburbs of Tappan, New York.  
-                    I have always been captivated by the endless possibilites presented by from coding and began learning Java,
-                    my first programming language, while in middle school. It all started at SummerTech, a sumemr camp that teaches 
-                    children basic coding concepts.  
-                </h2>
+                <h4>
+                    Hello! I'm 
+                    <span className="light-blue"> Ethan Pusch</span>,
+                    an aspiring software engineer and third-year student at 
+                    <span className="light-blue"> Northeastern University</span>.
+                    I'm currently based in Boston, MA, but I  grew up in the suburbs of Tappan, New York.  
+                    <br/> <br/>
+                    I have always been captivated by the endless possibilites presented by coding and began learning Java
+                    in middle school. It all started at 
+                    <span className="light-blue"> SummerTech</span>
+                    , a summer camp that teaches children basic coding concepts. 
+                    I would later become a teacher here, and my fascination with coding only accelerated as time went on. 
+                    As college approached, majoring in Computer Science seemed like my next logical step. 
+                    <br/> <br/>
+                    Influenced by the CEO of the 
+                    <span className="light-blue"> Xogene Services</span>
+                    , a company that I previously interned at, I decided to study 
+                    Business Administration as well. I was convinced that the theory and logic that I would obtain from 
+                    a Computer Science degree would be paramount to my future career, but that general Business knowledge is 
+                    indispensable as well. Therefore, a minor in Business was the perfect addition. 
+                    <br/> <br/>
+                    Northeastern made me realize that I have so much yet to learn in this ever expanding field. Although it was
+                    initially daunting, I dove in head first to the new technologies that I had not yet explored and my passion 
+                    for creating through code only continued to grow. When I am not debugging, you can find me at a field near by
+                    throwing a frisbee or practicing with my teammates on the Ultimate Frisbee team.
+                    <br/> <br/>
+                    Feel free to explore this website to see my recent projects
+                    and past work experiences. If you want to chat, do not hesitate to reach out!
+                </h4>
             </div>
         </div>
     );
--- src/pages/experience.js
+++ src/pages/experience.js
@@ -39,23 +39,33 @@ const Experience = () => {
                             <h4> January - June 2020 </h4>
                         </div>
                         <div className="experience-header-item">
-                            <Image src="/lenels2_logo.png" width={210} height={40}/>
+                            <Link href="https://www.lenels2.com/">
+                                <a className="no-decoration-link" target="_blank" rel="noopener noreferrer">
+                                    <Image src="/lenels2_logo.png" width={220} height={40}/>
+                                </a>
+                            </Link>
                         </div>
                     </div>
 
                     <p className="experience-item-description"> 
                         While at LenelS2, I worked as a part of their Video Management System (VMS) team. I contributed
-                        to their flagship video system product, the VRx. Upon joining, I was eager to contribute to VRx's
+                        to their flagship video system product, the VRx. 
+                        <br/> <br/>
+                        Upon joining, I was eager to contribute to VRx's
                         exciting, in progress feature--its real-time object detection based surveillence software. Although I 
                         have since taken a Computer Systems course (after my time with LenelS2), I was unfamiliar 
                         with their tech stack at the start of my co-op assignment. I had to quickly become familiar with 
-                        Ubuntu Linux and working strictly with the command line interface. After self-teaching REST commands
+                        Ubuntu Linux and working strictly with the command line interface. 
+                        <br/> <br/>
+                        After self-teaching REST commands
                         to interact with their APIs and daemons, I developed a variety of test platforms in Python, C#, and Bash. 
                         These utilities were used in order to support back-end features of the VRx. Despite the disruption that
                         the Covid-19 virus caused halfway through my co-op experience, I continued to work dilligently even after
                         transitioning to working remotely. While remote, I examined a threading issue that was affecting the performance
                         of a daemon related to camera analytics. I also created a logging function in C++ in order to output 
-                        important debugging information from the new data pipeline that LenelS2 was implementing. I am grateful 
+                        important debugging information from the new data pipeline that LenelS2 was implementing.
+                        <br/> <br/>
+                        I am grateful 
                         for my experience at LenelS2 and look forward to continuing to grow as a software engineer!
                     </p>
 
@@ -82,13 +92,19 @@ const Experience = () => {
                             <h4> June - August 2017 and 2018 </h4>
                         </div>
                         <div className="experience-header-item">
-                            <Image src="/xogene_logo_cropped.png" width={200} height={40}/>
+                            <Link href="https://www.xogene.com/">
+                                <a className="no-decoration-link" target="_blank" rel="noopener noreferrer">
+                                    <Image src="/xogene_logo_cropped.png" width={210} height={40}/>
+                                </a>
+                            </Link>
                         </div>
                     </div>
                     <p className="experience-item-description"> 
                         My internships at Xogene allowed me to get my feet wet in the world of software engineering, as this was my first
                         time working in an agile team development environment. I was still in highschool during this experience, so this
-                        opportunity was great as I realized how much I had yet to learn. Xogene is a startup company that has a light-weight,
+                        opportunity was great as I realized how much I had yet to learn. 
+                        <br/> <br/>
+                        Xogene is a startup company that has a light-weight,
                         but strong software team. Due to the small company size, I was able to directly communicate with higher-ups and gain
                         exposure to our front end features in a much closer way than I would have been able to at another company. While here,
                         I developed cross browser test scripts using Python and Selenium in order to verify their new front end features.
@@ -119,17 +135,25 @@ const Experience = () => {
                             <h4> June - August 2016 </h4>
                         </div>
                         <div className="experience-header-item">
-                            <Image src="/summertech_logo.png" width={150} height={75}/>
+                            <Link href="https://www.summertech.net/">
+                                <a className="no-decoration-link" target="_blank" rel="noopener noreferrer">
+                                    <Image src="/summertech_logo.png" width={150} height={75}/>
+                                </a>
+                            </Link>
                         </div>
                     </div>
                     <p className="experience-item-description"> 
                         This is one of the most fun work experiences that I have had. SummerTech is a summer camp that I used to attend,
-                        and it was here where I initally learned how to code. I chose to learn Java as my first language because it was 
-                        the most popular course while I was attending. Once I was old enough, I was applied to become a teacher.
+                        and it was here where I initally learned how to code. 
+                        <br/> <br/>
+                        I chose to learn Java as my first language because it was 
+                        the most popular course while I was attending. Once I was old enough, I applied to become a teacher.
                         With all of the Java experience that I had under my belt from attending the camp for years, I was offered my first 
                         job as a junior Java teacher. Over the course of 5 weeks, I was given 2-4 students to teach each week. Each 
                         student had varying levels of experience with coding, and I had to assign projects based on their
-                        respective proficiencies with Java. Outside of class, I was a counselour who was tasked with other duties
+                        respective proficiencies with Java. 
+                        <br/> <br/>
+                        Outside of class, I was a counselour who was tasked with other duties
                         related to ensuring entertainment and safety for the students in their extra cirricular activites while at SummerTech.
                     </p>
                     <div className="tech-stack-list">
